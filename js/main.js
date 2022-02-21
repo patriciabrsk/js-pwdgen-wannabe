2317d1f0381a400432aff1f7bc2c32d50e9e84ae
patriciabrsk <patriciabresku@gmail.com>
2022-02-21 16:23:28 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const name = prompt("What's your name?", "Name");
+const lastname = prompt("What's your last name?", "Last name");
+const color = prompt("What's your favorite color?", "Color");
+
+
+document.getElementById("password").innerHTML = name + lastname + color + 21;
